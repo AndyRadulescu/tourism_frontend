204a5fa10de543de3ecaf2aa6a55f6091277af85
AndyRadulescu <radulescu.eduard.andrei@gmail.com>
2019-01-26 15:48:10 +0100

    created the list for about me

--- src/app/main/aboutme/aboutme.component.ts
+++ src/app/main/aboutme/aboutme.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {UserServiceService} from '../user-service.service';
+import Room, {RoomObject} from '../../model/Room';
 
 @Component({
     selector: 'app-aboutme',
@@ -8,14 +9,24 @@ import {UserServiceService} from '../user-service.service';
 })
 export class AboutmeComponent implements OnInit {
 
+    public hasRooms = false;
+    public didLoad = false;
+    public roomsArray: Room[];
+
     constructor(private userService: UserServiceService) {
     }
 
     ngOnInit() {
         this.userService.sync();
-        this.userService.getRooms().subscribe(rooms => {
+        this.userService.getRooms().subscribe((rooms: RoomObject) => {
             console.log(rooms);
+            this.roomsArray = rooms.roomList;
+            this.hasRooms = !!rooms;
+            this.didLoad = true;
+        }, (e) => {
+            this.didLoad = true;
         });
     }
+
 
 }
--- src/app/model/Room.ts
+++ src/app/model/Room.ts
@@ -1,57 +1,15 @@
-export default class Room {
-    private _id: String;
-    private _number: String;
-    private _description: String;
-    private _startDate: String;
-    private _endDate: String;
+import Hotel from './Hotel';
 
+export default interface Room {
+    id: String;
+    number: String;
+    description: String;
+    startDate: String;
+    endDate: String;
+    hotel_id: String;
+    hotel: Hotel;
+}
 
-    constructor(id?: String, number?: String, description?: String, startDate?: String, endDate?: String) {
-        this._id = id;
-        this._number = number;
-        this._description = description;
-        this._startDate = startDate;
-        this._endDate = endDate;
-    }
-
-
-    get id(): String {
-        return this._id;
-    }
-
-    set id(value: String) {
-        this._id = value;
-    }
-
-    get number(): String {
-        return this._number;
-    }
-
-    set number(value: String) {
-        this._number = value;
-    }
-
-    get description(): String {
-        return this._description;
-    }
-
-    set description(value: String) {
-        this._description = value;
-    }
-
-    get startDate(): String {
-        return this._startDate;
-    }
-
-    set startDate(value: String) {
-        this._startDate = value;
-    }
-
-    get endDate(): String {
-        return this._endDate;
-    }
-
-    set endDate(value: String) {
-        this._endDate = value;
-    }
+export interface RoomObject {
+    roomList: Room[];
 }
